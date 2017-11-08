ecc419463e852bf9d0c1a51a09d1da86be0c7ae7
Katie-Loyd <katiecameronloyd@gmail.com>
2017-11-08 16:16:27 -0500

    Reformatted for plain text output

--- newspaper.py
+++ newspaper.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 import psycopg2
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-root_page = '''<!DOCTYPE html>
-    <title>Newspaper Analytics Report</title>
-      <pre>
-          {}
-      </pre>'''
 
 DBNAME = "news"
 
@@ -71,40 +64,14 @@ def get_report():
     art = get_pop_articles()
     auth = get_pop_authors()
     err = get_error_days()
-    final = '''<h2>Top 3 Articles</h2>
-        <ol>
-            <li>{} -- {} views</li>
-            <li>{} -- {} views</li>
-            <li>{} -- {} views</li>
-        </ol>
-        <h2>Most Popular Authors</h2>
-        <ol>
-            <li>{} -- {} views</li>
-            <li>{} -- {} views</li>
-            <li>{} -- {} views</li>
-            <li>{} -- {} views</li>
-        </ol>
-        <h2>Days With More Than 1% Error Rate</h2>
-        <ol>
-            <li>{} -- {}% errors</li>
-        </ol>
-        '''.format(
-            art[0][0], art[0][1], art[1][0], art[1][1],
-            art[2][0], art[2][1], auth[0][0], auth[0][1],
-            auth[1][0], auth[1][1], auth[2][0], auth[2][1],
-            auth[3][0], auth[3][1], err[0][0], err[0][1])
-    return final
+    print("Top 3 Articles")
+    for ar in art:
+        print("- {} -- {} views".format(ar[0], ar[1]))
+    print("\nMost Popular Authors")
+    for au in auth:
+        print("- {} -- {} views".format(au[0], au[1]))
+    print("\nDays With More Than 1% Error Rate")
+    for e in err:
+        print("- {} -- {}% errors".format(e[0], e[1]))
 
-
-class ReportGenerator(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html; charset=utf-8')
-        self.end_headers()
-        report = root_page.format(get_report())
-        self.wfile.write(report.encode())
-
-if __name__ == '__main__':
-    server_address = ('', 8000)
-    httpd = HTTPServer(server_address, ReportGenerator)
-    httpd.serve_forever()
+get_report()
