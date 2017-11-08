492b30df7959a5c07ab70195d6b340ba9a4076dd
Katie-Loyd <katiecameronloyd@gmail.com>
2017-11-08 12:06:24 -0500

    Added server start-up function

--- newspaper.py
+++ newspaper.py
@@ -55,3 +55,8 @@ def get_error_days():
     days = c.fetchall()
     db.close()
     return days
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, MessageHandler)
+    httpd.serve_forever()
