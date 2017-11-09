17bd4df3d857219786b99f43ad6b47205d30f3d3
Katie-Loyd <katiecameronloyd@gmail.com>
2017-11-08 21:32:33 -0500

    Simplified error select statement

--- newspaper.py
+++ newspaper.py
@@ -44,18 +44,18 @@ def get_error_days():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute(
-        '''select daily_errors.date,
+        '''select to_char(daily_errors.date, 'FMMonth FMDD, YYYY') as date,
         round(daily_errors.sum / daily_logs.count * 100, 1) as percent
-        from (select to_char(time, 'FMMonth FMDD, YYYY') as date, sum(errors)
-        from (select time, count(*) as errors from log
-        where status like '4%' group by time) as error_list
-        group by date
-        order by date) as daily_errors,
-        (select to_char(time, 'FMMonth FMDD, YYYY') as date, count(id)
-        from log group by date order by date) as daily_logs
-        where daily_errors.date = daily_logs.date and
-        round(daily_errors.sum / daily_logs.count * 100, 1) > 1
-        order by daily_errors.date''')
+        from (select date(time) as date, count(*)::decimal as sum
+        from log
+        where status like '4%'
+        group by date(time)) as daily_errors,
+        (select date(time) as date, count(id)
+        from log
+        group by date(time)) as daily_logs
+        where daily_errors.date = daily_logs.date
+        and round(daily_errors.sum / daily_logs.count * 100, 1) > 1
+        order by daily_errors.date;''')
     days = c.fetchall()
     db.close()
     return days
